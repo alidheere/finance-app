3aff2040807dc64f8f900db9def313438a0a59f4
alidheere <sakajr33@gmail.com>
2026-09-18 08:45:19 -0700

    all

--- index.js
+++ index.js
@@ -39,6 +39,17 @@ app.use("/", uploadRouters);
 app.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(errorHandler);
 
+const mongoURI =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_URI_DEV
+    : process.env.MONGO_URI_PRO;
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("Mongo URI exists:", !!mongoURI);
+
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 mongoose
    .connect(process.env.NODE_ENV === "development"?  process.env.MONGO_URI_DEV: process.env.MONGO_URI_PRO)
   .then(() => {
@@ -51,3 +62,11 @@ mongoose
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
+
+
+
+
+
+
+
+
