f4090a9b2b05418a8142aea8da3cb83c14f7460d
alidheere <sakajr33@gmail.com>
2026-09-18 08:24:30 -0700

    all changes

--- utils/swagger.js
+++ utils/swagger.js
@@ -15,7 +15,7 @@ const options = {
     url:
       process.env.NODE_ENV === "development"
         ? "http://localhost:5000"
-        : "https://transantionapp-1.onrender.com",
+        : "https://transantionapp.onrender.com",
   },
 ],
     components: {
